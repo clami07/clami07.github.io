b0231dc70e793804c531f1b9eec6c2d56a012c08
Yamilka <clami07@yahoo.es>
2018-02-24 21:21:44 -0700

    update-lesson-7

--- assignments/lesson-7/scripts/windchill.js
+++ assignments/lesson-7/scripts/windchill.js
@@ -1,6 +1,9 @@
 function inputData() {
-    windSpeed = 5;
-    avgTemperture = (90+66)/2;
+    var highTemp = parseInt(document.getElementById("hight").innerHTML);
+    var lowTemp = parseInt(document.getElementById("lowt").innerHTML);
+var windSpeed = parseInt(document.getElementById("winds").innerHTML);
+    
+    avgTemperture = (highTemp+lowTemp)/2;
     
     var storeValue = windChill(windSpeed, avgTemperture);
     
@@ -12,7 +15,7 @@ function windChill(windSpeed, avgTemperture){
 var s = windSpeed;
 var t = avgTemperture;
 
-var windChillFactor = 35.74 + 0.6215 * avgTemperture - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * t * Math.pow(windSpeed, 0.16);
+var windChillFactor = 35.74 + 0.6215 * avgTemperture - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16);
 var digits = 2;
 var multiplier = Math.pow(10, digits);
 windChillFactor = Math.round (windChillFactor * multiplier) / multiplier;
