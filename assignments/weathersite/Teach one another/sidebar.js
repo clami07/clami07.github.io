13cb3a37afc694e9bca9d35a26d04dc7dc8e8ead
Yamilka <clami07@yahoo.es>
2018-03-17 22:55:44 -0600

    adding-new-pages

---
+++ assignments/weathersite/Teach one another/sidebar.js
@@ -0,0 +1,20 @@
+// side bar
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+
+// Tob bar with Home and Hamburguer
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
