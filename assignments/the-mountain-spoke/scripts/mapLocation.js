d506796707b08a5f32aa4e8a24596e45b282f241
Yamilka <clami07@yahoo.es>
2018-04-14 08:57:22 -0600

    updating

--- assignments/the-mountain-spoke/scripts/mapLocation.js
+++ assignments/the-mountain-spoke/scripts/mapLocation.js
@@ -1,10 +1,21 @@
-{src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDoeoEnttJdgZIwP5CCJN0_qIoRO8IqefM&origin=place_id: ChIJ37Yst5pFRVMRiOpIlo-WmLs&sensor=true"
+/*src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDoeoEnttJdgZIwP5CCJN0_qIoRO8IqefM&q=Bozeman,MT"*/
 
-function initialize(){
+/*
     var mapOptions = {
-        center: new google.maps.LatLng(-34.397, 150.644),
+        center: new google.maps.LatLng(45.672490, -111.090685),
+        zoom: 10,
+        mapTypeId: google.maps.MapTypeId.place
+    }
+var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
+ 
+ */
+ 
+
+function initMap(){
+    var mapOptions = {
+        center:{lat:45.672490, lng:-111.090685}
         zoom:8,
         mapTypeId: google.maps.MapType.place};
-    var map = new google.maps.Map(document.getElementsByClassName('map'), mapOptions);
+    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
     }
-};
