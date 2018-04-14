4e20c14e0614b9d9a2f5a322aca51ba2a5b0677c
Yamilka <clami07@yahoo.es>
2018-04-14 16:19:43 -0600

    updating

---
+++ assignments/the-mountain-spoke/scripts/servicesjs.js
@@ -0,0 +1,28 @@
+
+var servicesData = JSON.parse(services.json);
+
+function populateSection(servicesData){
+    var table = document.servicesData['TuneUpsBuilds', 'Wheels', 'Brakes', 'Handlebars/ForkHeadset'];
+    
+    for (var i=0; i< servicesData.length; i++){
+        var myTable = document.createElement('table');
+        var tr1 = document.createElement('tr');
+        var tr2 = document.createElement('tr');
+        var tr3 = document.createElement('tr');
+        var tr4 = document.createElement('tr');
+        var tr5 = document.createElement('tr');
+        var tr6 = document.createElement('tr');
+        var tr7 = document.createElement('tr');
+        var tr8 = document.createElement('tr');
+        var tr9 = document.createElement('tr');
+        var tr10 = document.createElement('tr');
+        var tr11 = document.createElement('tr');
+        var td1 = document.createElement('td');
+        var tr13 = document.createElement('tr');
+        var tr1 = document.createElement('tr');
+        var th = document.createElement('th');
+        var td = document.createElement('td');
+        
+        
+    }
+}
